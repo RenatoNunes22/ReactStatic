cb7003ae6f7db17e02290d6c03e79f436c2a7a64
Renato Nunes <97243555+RenatoNunes22@users.noreply.github.com>
2022-07-12 20:18:42 -0300

    inserção-logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
-
-import App from "./App";
+import Cardapio from "./pages/Cardapio";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Cardapio />
   </StrictMode>
 );
